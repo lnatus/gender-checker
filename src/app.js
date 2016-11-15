003c598e553531e9c008597ea5c61c98c5d1485b
lnatus <l.natus@live.com>
2016-11-15 16:39:56 +0100

    Fix cert path for uberspace

--- src/app.js
+++ src/app.js
@@ -2,20 +2,23 @@ var mongoose = require('mongoose');
 var express = require('express');
 var https = require('https');
 var http = require('http');
+var path = require('path');
 var fs = require('fs');
 
 var api = require('./routes/api');
 
 var options = {
-   key: fs.readFileSync('../cert/key.pem'),
-   cert: fs.readFileSync('../cert/cert.pem')
+   key: fs.readFileSync(path.join(__dirname, '../cert') + '/key.pem'),
+   cert: fs.readFileSync(path.join(__dirname, '../cert') + '/cert.pem')
 };
 
 var app = express();
 
 app.get('/', api.read);
 
+//TODO: Config pev / prod 
 mongoose.connect('mongodb://localhost/gcdb');
+//mongoose.connect('mongodb://gc_mongoadmin:boo6XoaSha@localhost:21199/admin');
 
-http.createServer(app).listen(3000);
-https.createServer(options, app).listen(8000);
+http.createServer(app).listen(61000);
+https.createServer(options, app).listen(61001);
