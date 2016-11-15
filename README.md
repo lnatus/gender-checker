2dc165c2181c2d2586341d08efc6dfd73350cce4
lnatus <l.natus@live.com>
2016-11-15 21:11:54 +0100

    Basic read.me

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # gender-checker
+
 Gender Checker API
+
+## API Usage
+
+### Sample request
+
+`http:\\server\api?apiKey=B567228F-56C0-416D-BE1A-44820CFBEF22&name=Max`
+
+	* apiKey (Required)
+	* name (Required)
+
+### Sample response
+
+`{ name : "Max", gender : "m"}`
+
+## Error Codes
+
+### Error response
+
+`{ "code" : 100, "error" : "apiKey parameter not found" }
+
+### General Error Codes
+
+	*	1 Unkown API Error
+	* 404 Route not found
+
+### API Key Error Codes
+
+	*	100 apiKey parameter not found
+	*	101	apiKey is invalid
+	*	102 Could not find subscription for this apiKey
+	*	103 Subcription for this apiKey is expired
+
+### Name Error Codes
+
+	* 200 name parameter not found
+
+### Limit Error Codes
+
+	* 300 Monthly request limit exceeded
--- src/config/error.js
+++ src/config/error.js
@@ -2,11 +2,11 @@ var error = {
 	api : { code : 1, error : 'Gender API Error. Please try again or report that issue at contact [at] gender-checker.com'},
 	apiKeyMissing : { code : 100, error : 'apiKey parameter not found' },
 	apiKeyInvalid : { code : 101, error : 'apiKey parmater is invalid'},
-	apiKeySubscription: { code : 102, error : 'Could not find subscription for this apiKey.'},
-	apiSubscriptionExpired: { code: 103, error : 'Subscription for this apiKey is expired.'},
+	apiKeySubscription: { code : 102, error : 'Could not find subscription for this apiKey'},
+	apiSubscriptionExpired: { code: 103, error : 'Subscription for this apiKey is expired'},
 	nameMissing : { code : 200, error : 'name parameter not found' },
-	monthlyLimitExceeded : { code : 300, error : 'Monthly request limit exceeded.'},
-	notFound: { code : 404, error : 'Wrong request. This route can not be found.' }
+	monthlyLimitExceeded : { code : 300, error : 'Monthly request limit exceeded'},
+	notFound: { code : 404, error : 'Wrong request. This route can not be found' }
 }
 
 module.exports = error;
